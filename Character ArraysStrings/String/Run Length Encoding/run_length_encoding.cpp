a9b34e41805ada0ab96b69da57399a205b4f6bf1
Tayeb Ahmed <atayeb.english939@gmail.com>
2024-08-15 13:01:22 +0600

    String Compression

--- Character ArraysStrings/String/Run Length Encoding/run_length_encoding.cpp
+++ Character ArraysStrings/String/Run Length Encoding/run_length_encoding.cpp
@@ -29,7 +29,7 @@ string stringCompressed(string str) {
                                 // is used to convert integer to string
   }
 
-  if (output.length() < str.length()) {
+  if (output.length() > str.length()) {
     return str;
   }
   return output;
@@ -40,7 +40,7 @@ int main() {
   string s1 = "aaabbccddee";
   cout << stringCompressed(s1) << endl;
 
-  string s2 = "aabbcd";
+  string s2 = "abcd";
   cout << stringCompressed(s2) << endl;
 
   return 0;

480fef8bfd8b19e0ec88e5746229937954da6193
Tayeb Ahmed <atayeb.english939@gmail.com>
2024-08-15 12:47:17 +0600

    String

---
+++ Character ArraysStrings/String/Run Length Encoding/run_length_encoding.cpp
@@ -0,0 +1,47 @@
+// Run Length Encoding for String Compression
+// if compressed string is bigger than original string, return original string
+// Input  : aaabbccddee
+// Output : a3b2c2d2e2
+
+// Input - 2  : abcd
+// Output - 2 : abcd
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string stringCompressed(string str) {
+  int n = str.length(); // length of string
+
+  string output; // For storing compressed string
+
+  for (int i = 0; i < n; i++) {
+    int count = 1; // Initialize count to 1
+
+    while (i < n - 1 and str[i + 1] == str[i]) {
+      count++;
+      i++;
+    }
+
+    output += str[i];           // Here += is for concatenation
+    output += to_string(count); // Here += is for concatenation and to_string()
+                                // is used to convert integer to string
+  }
+
+  if (output.length() > n) {
+    return str;
+  }
+  return output;
+}
+
+int main() {
+
+  string s1 = "aaabbccddee";
+  cout << stringCompressed(s1) << endl;
+
+  string s2 = "abcd";
+  cout << stringCompressed(s2) << endl;
+
+  return 0;
+}

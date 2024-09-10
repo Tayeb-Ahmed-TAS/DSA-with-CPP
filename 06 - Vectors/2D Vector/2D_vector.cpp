8464ae6ed6dc371f6c1f0083436cb35e04b1889f
Tayeb Ahmed <atayeb.english939@gmail.com>
2024-09-10 10:55:54 +0600

    2D Vector

--- 06 - Vectors/2D Vector/2D_vector.cpp
+++ 06 - Vectors/2D Vector/2D_vector.cpp
@@ -1,10 +1,56 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(){
+int main() {
 
-    
+  // 2D Vector
 
-    return 0;
+  vector<vector<int>> arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9, 10}, {11, 12}};
+
+  // Print all elements of the vector
+
+  cout << "The 2D vector is: " << endl;
+
+  for (int i = 0; i < arr.size(); i++) {
+    // arr.size() gives the number of rows
+    for (int j = 0; j < arr[i].size(); j++) {
+      // arr[i].size() gives the number of columns in the ith row
+      cout << arr[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+  // Update the element at 2nd row and 3rd column
+
+  arr[1][2] += 10;
+
+  cout << "The updated 2D vector is: " << endl;
+
+  // Print all elements of the vector using for each loop
+
+  for (auto row : arr) {
+    for (auto col : row) {
+      cout << col << " ";
+    }
+    cout << endl;
+  }
+
+  // Add a new row to the 2D vector
+
+  arr.push_back({13, 14, 69});
+
+  cout << "The 2D vector after adding a new row is: " << endl;
+
+  // Print all elements of the vector using range based for loop
+
+  for (int i = 0; i < arr.size(); i++) {
+    for (int number : arr[i]) {
+      cout << number << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
 }
--- 06 - Vectors/2D Vector/README.md
+++ 06 - Vectors/2D Vector/README.md
@@ -49,6 +49,27 @@ for(int i = 0; i < v.size(); i++) {
 
 ```
 
+### Printing using for each loop
+
+You can also use a for each loop to iterate over a 2D vector.
+
+```cpp
+
+vector<vector<int>> v = {
+                            {1, 2, 3},
+                            {4, 5, 6},
+                            {7, 8, 9}
+                        };
+
+for(auto row : v) {
+    for(auto col : row) {
+        cout << col << " ";
+    }
+    cout << endl;
+}
+
+```
+
 ### Note
 
 It's not necessary that all the rows of a 2D vector have the same number of columns. You can have a 2D vector with different number of columns in each row.
